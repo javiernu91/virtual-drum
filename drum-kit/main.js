11e5159e05d0b7aed086c3d06e7420efa62fb775
Javier Nuñez <javiernu16@gmail.com>
2023-12-28 14:27:34 -0500

    add the function to remove the transform style

--- drum-kit/main.js
+++ drum-kit/main.js
@@ -1,6 +1,26 @@
-const drum = document.querySelector("body")
+const drum = document.querySelector("body");
+
+const keys = document.querySelectorAll(".key")
 const keyA = document.querySelector("#keyA");
+const keyS = document.querySelector("#keyS");
+const keyD = document.querySelector("#keyD");
+const keyF = document.querySelector("#keyF");
+const keyG = document.querySelector("#keyG");
+const keyH = document.querySelector("#keyH");
+const keyJ = document.querySelector("#keyJ");
+const keyK = document.querySelector("#keyK");
+const keyL = document.querySelector("#keyL");
+
+
 const audioA = document.querySelector("#audioA");
+const audioS = document.querySelector("#audioS");
+const audioD = document.querySelector("#audioD");
+const audioF = document.querySelector("#audioF");
+const audioG = document.querySelector("#audioG");
+const audioH = document.querySelector("#audioH");
+const audioJ = document.querySelector("#audioJ");
+const audioK = document.querySelector("#audioK");
+const audioL = document.querySelector("#audioL");
 
 
 
@@ -15,34 +35,60 @@ drum.addEventListener("keypress", (e) => {
 })
 
 function playSound(code) {
-  if (code == "a") {
-    keyA.classList.toggle("playing");
+  if (code == "a" || code == "A") {
+    audioA.currentTime = 0;
+    keyA.classList.add("playing");
     audioA.play()
     console.log("clap");
     console.log(code);
     document.body.appendChild(audioA);
-  } else if (code == "s") {
+  } else if (code == "s" || code == "S") {
+    keyS.classList.add("playing");
     console.log ("hihat") 
     console.log(code);
-  } else if (code == "d") {
+    audioS.currentTime = 0;
+    audioS.play()
+    document.body.appendChild(audioS);
+  } else if (code == "d" || code == "D") {
+    keyD.classList.add("playing");
+    audioD.currentTime = 0;
+    audioD.play();
     console.log ("kick") 
     console.log(code);
-  } else if (code == "f") {
+  } else if (code == "f" || code == "F") {
+    keyF.classList.add("playing")
+    audioF.currentTime = 0;
+    audioF.play();
     console.log ("openhat") 
     console.log(code);
-  } else if (code == "g") {
+  } else if (code == "g" || code == "G") {
+    keyG.classList.add("playing");
+    audioG.currentTime = 0;
+    audioG.play();
     console.log ("boom") 
     console.log(code);
-  } else if (code == "h") {
+  } else if (code == "h" || code == "H") {
+    keyH.classList.add("playing")
+    audioH.currentTime = 0;
+    audioH.play();
     console.log ("ride") 
     console.log(code);
-  } else if (code == "j") {
+  } else if (code == "j" || code == "J") {
+    keyJ.classList.add("playing")
+    audioJ.currentTime = 0;
+    audioJ.play();
     console.log ("snare") 
     console.log(code);
-  } else if (code == "k") {
+  } else if (code == "k" || code == "K") {
+    keyK.classList.add("playing")
+    audioK.currentTime = 0;
+    audioK.play();
     console.log ("tom") 
     console.log(code);
-  } else if (code == "l") {
+  } else if (code == "l" || code == "L") {
+    keyL.classList.add("playing")
+    audioL.currentTime = 0;
+    audioL.play();
     console.log ("tink") 
     console.log(code);
   } 
@@ -51,6 +97,10 @@ function playSound(code) {
   }
 }
 
-// function addClass (e) {
-//    =  
-// }
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; //skit it if it's not a transform
+  this.classList.remove('playing');
+}
+
+// const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
